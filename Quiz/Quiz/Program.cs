08062eaca553492ae929b89c40bae72bb99f9849
Novikova Gailna <sollinka@mail.ru>
2017-03-26 21:52:11 +0300

    Quiz was created

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какой сейчас год?");
+            string answer1 = Console.ReadLine();
+
+            if (answer1 == "2017")
+            {
+                Console.WriteLine("Верно.");
+            }
+            else
+            {
+                Console.WriteLine("Неверно.");
+            }
+            Console.Read();
+        }
+    }
+}
