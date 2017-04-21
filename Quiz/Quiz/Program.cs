fe720b83c94acb29959497eed13fd73e53e65471
Novikova Galina <sollinka@mail.ru>
2017-04-21 23:43:12 +0300

    New three questions implemented.

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -11,9 +11,9 @@ namespace Quiz
         static void Main(string[] args)
         {
             Console.WriteLine("Какой сейчас год?");
-            string answer1 = Console.ReadLine();
+            int answer1 = int.Parse(Console.ReadLine());
 
-            if (answer1 == "2017")
+            if (answer1 == 2017)
             {
                 Console.WriteLine("Верно.");
             }
@@ -21,8 +21,44 @@ namespace Quiz
             {
                 Console.WriteLine("Неверно.");
             }
-            Console.Read();
-			
+
+
+            Console.WriteLine("Сколько сейчас минут?");
+            int answer2 = int.Parse(Console.ReadLine());
+
+            if (answer2 == DateTime.Now.Minute)
+            {
+                Console.WriteLine("Верно.");
+            }
+            else
+            {
+                Console.WriteLine("Неверно.");
+            }
+
+            Console.WriteLine("Напишите текуший месяц.");
+            int answer3 = int.Parse(Console.ReadLine());
+
+            if (answer3 == DateTime.Now.Month)
+            {
+                Console.WriteLine("Верно.");
+            }
+            else
+            {
+                Console.WriteLine("Неверно.");
+            }
+
+            Console.WriteLine("В каком регионе нашей страны мы находимся?");
+            int answer4 = int.Parse(Console.ReadLine());
+
+            if (answer4 == 52)
+            {
+                Console.WriteLine("Верно.");
+            }
+            else
+            {
+                Console.WriteLine("Неверно.");
+            }
+            Console.ReadLine();
         }
-    }
+    } 
 }
